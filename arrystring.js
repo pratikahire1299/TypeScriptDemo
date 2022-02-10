5c8c58c295096166e639b047949ee97175ce03e2
Pratik Ahire <pratikahire522@gmail.com>
2022-02-10 12:14:39 +0530

    New Branch created and files added

---
+++ arrystring.js
@@ -0,0 +1,7 @@
+
+const specialForces=["BlackCats","Marcos", "Demolishers","HeadHunters"];  
+if(!specialForces.includes("HeadHuners")){
+    console.log("False")
+}  else{
+    console.log("True")
+}
